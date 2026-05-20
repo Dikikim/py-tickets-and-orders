1d87ccb72a162d171c33ff75007509f2ea2a3368
Denis <dikikim@gmail.com>
2026-05-20 14:37:06 +0300

    'Solution'

--- services/movie.py
+++ services/movie.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from django.db.models import QuerySet
 
 from db.models import Movie
@@ -6,7 +7,8 @@ from db.models import Movie
 def get_movies(
     genres_ids: list[int] = None,
     actors_ids: list[int] = None,
-) -> QuerySet:
+    title: str = None,
+) -> QuerySet[Movie]:
     queryset = Movie.objects.all()
 
     if genres_ids:
@@ -15,13 +17,19 @@ def get_movies(
     if actors_ids:
         queryset = queryset.filter(actors__id__in=actors_ids)
 
+    if title:
+        queryset = queryset.filter(title__icontains=title)
+
     return queryset
 
 
-def get_movie_by_id(movie_id: int) -> Movie:
+def get_movie_by_id(
+    movie_id: int,
+) -> Movie:
     return Movie.objects.get(id=movie_id)
 
 
+@transaction.atomic
 def create_movie(
     movie_title: str,
     movie_description: str,
@@ -38,3 +46,4 @@ def create_movie(
         movie.actors.set(actors_ids)
 
     return movie
+
--- services/movie_session.py
+++ services/movie_session.py
@@ -1,10 +1,12 @@
 from django.db.models import QuerySet
 
-from db.models import MovieSession
+from db.models import MovieSession, Ticket
 
 
 def create_movie_session(
-    movie_show_time: str, movie_id: int, cinema_hall_id: int
+    movie_show_time: str,
+    movie_id: int,
+    cinema_hall_id: int,
 ) -> MovieSession:
     return MovieSession.objects.create(
         show_time=movie_show_time,
@@ -13,14 +15,18 @@ def create_movie_session(
     )
 
 
-def get_movies_sessions(session_date: str = None) -> QuerySet:
+def get_movies_sessions(
+    session_date: str = None,
+) -> QuerySet[MovieSession]:
     queryset = MovieSession.objects.all()
     if session_date:
         queryset = queryset.filter(show_time__date=session_date)
     return queryset
 
 
-def get_movie_session_by_id(movie_session_id: int) -> MovieSession:
+def get_movie_session_by_id(
+    movie_session_id: int,
+) -> MovieSession:
     return MovieSession.objects.get(id=movie_session_id)
 
 
@@ -40,5 +46,21 @@ def update_movie_session(
     movie_session.save()
 
 
-def delete_movie_session_by_id(session_id: int) -> None:
+def delete_movie_session_by_id(
+    session_id: int,
+) -> None:
     MovieSession.objects.get(id=session_id).delete()
+
+
+def get_taken_seats(
+    movie_session_id: int,
+) -> list[dict]:
+    return list(
+        Ticket.objects.filter(
+            movie_session_id=movie_session_id,
+        ).values(
+            "row",
+            "seat",
+        )
+    )
+
